7bba493434fc8d9b15139de727bde96e4403c698
Andres <a.castrocabrera@ugto.mx>
2019-05-06 20:00:11 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,300 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>NotiTorti</title>
+  </head>
+  <body style="background-color: #B3B6B7;">
+  <nav class="navbar navbar-light sticky-top" style="background-color: #C8170B;">
+    <a class="navbar-brand" href="#">
+      <img src="44027.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+      NotiTorti
+  </a>
+  <ul class="nav justify-content-end">
+    <li class="nav-item">
+      <a class="nav-link active" style="background-color: #566573; color: #ECF0F1;" href="#acercaDIV">Acerca de</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" style="background-color: #566573; color: #ECF0F1;" href="#contactoDiv">Contacto</a>
+    </li>
+</nav>
+
+<!-- Presentacion -->
+<div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="HuachiQuemado.gif" height="500" class="d-block w-100" alt="Responsive image">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Salamanca Guanajuato</h5>
+          <a href="javascript:muestraVideo(500)" >CMAPAS acusada de huachicoleo.</a>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="animacion-ondas-gravitacionales.gif" height="500" class="d-block w-100" alt="Responsive image">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Ciudad de México</h5>
+          <a href="javascript:muestraOnda(500)">Ondas gravitacionales activan alerta sísmica.</a>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="Elon-Musk-full-metal-alchemist-twitter-rampage.jpg" height="500" class="d-block w-100" alt="Responsive image">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Internacional</h5>
+          <a href="javascript:muestraElon(500)">Elon Musk weeaboo!? Aquí te lo contamos todo.</a>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="Kim-jong-army-596781.jpg" height="500" class="d-block w-100" alt="Responsive image">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Internacional</h5>
+          <p>Famoso cantante de K-Pop es llamado al servicio militar.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="tumblr_p0q15rrGgL1qjczh3o1_500.gif" height="500" class="d-block w-100" alt="Responsive image">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Guanajuato Capital</h5>
+          <p>La beca no llega!, estudiantes entran en panico ya que iban a usar la beca para alcohol.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<!-- Informacion -->
+<div id="videoDiv" class="embed-responsive embed-responsive-21by9" style="display:none">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/F3wZ3gaywug?start=117" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+</div>
+
+<div id="sismoDIV" style="display:none">
+<p class="popout text-center"><span>¿Que es una onda gravitacional?, ¿Se puede meter en un bolillo?</span></p>
+<p class="text-center" face="Comic Sans MS,arial" >La relatividad general es una teoría de la gravedad que resulta compatible con la relatividad especial en muchos aspectos y, en particular, con el principio de que nada viaja más rápido que la luz. Esto significa que los cambios en el campo gravitatorio no pueden ocurrir en todas partes instantáneamente: deben propagarse. En la relatividad general se propagan a exactamente la misma velocidad que las ondas electromagnéticas por el vacío: a la velocidad de la luz. A estos cambios que se propagan se les llama ondas gravitatorias.
+La radiación gravitatoria es una predicción central de la relatividad general y su detección es una prueba clave de la integridad de la estructura teórica de la obra de Einstein. Sin embargo, es probable que en el largo plazo sea aún más importante como instrumento para la observación astronómica. Las observaciones del sistema púlsar binario Hulse-Taylor han proporcionado excelentes evidencias de que las predicciones de la relatividad general sobre la radiación gravitatoria son cuantitativamente correctas. Aun así, la información de la astronomía sobre las posibles fuentes de radiación detectable es incompleta.</p>
+<img src="Binary_system_coalescence.gif" height="500" class="rounded mx-auto d-block w-10 center-block img-fluid" alt="Responsive image">
+</div>
+
+<div id="animeDiv" style="display:none">
+<p class="popout text-center"><span>El autopiloto de los Tesla fue hecho para ver anime en la carretera.</span></p>
+<p class="text-center">Musk a menudo comparte sus reflexiones por medio de twitter, es así como hace un tiempo reveló que era un fanático del animé. Tal parece que su afición crece día con día, al punto de adaptar una de las prácticas más recurrentes entre los fans: usar a su personaje favorito como ícono de su perfil.
+Edward Elric, protagonista de Fullmetal Alchemist, fue el personaje que Elon eligió para que lo representará en esta red social, sorprendiendo a los fanáticos de esta serie y del animé en general.
+Ahora, los usuarios se encuentran preocupados de que el empresario investigue la transmutación humana, (práctica mediante la cual se crea un cuerpo artificial por medio de la alquimia) una de las características de este animé.
+Cabe destacar que no es la primera vez que el multimillonario hace una referencia a este género de la animación.</p>
+<img style="justify-content: center;" src="rtlv5brij4421.png" height="500" class="rounded mx-auto d-block w-10 center-block img-fluid" alt="Responsive image">
+
+</div>
+<!-- Contacto -->
+<div id="contactoDiv" style="width:100%; display: inline-block" class="list-group">
+  <a class="list-group-item list-group-item-action">Contacto</a>
+  
+  <a href="https://www.facebook.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-primary">
+  <img src="iconmonstr-facebook-6.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  Facebook oficial.</a>
+
+  <a href="tel:4641234567" class="list-group-item list-group-item-action list-group-item-secondary">
+  <img src="phone.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  Telefono: 4641234567</a>
+
+  <a href="https://twitter.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">
+  <img src= "img_76219.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  Twitter oficial</a>
+
+  <a class="list-group-item list-group-item-action list-group-item-secondary">
+  <img src="97805.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  Dirección: Enrique Segoviano</a>
+  
+ </div>
+
+<!-- Acerca de -->
+
+<div id="acercaDIV" class="card mb-3">
+  <div class="card-body">
+    <h4 class="card-title">Acerca de</h4>
+    <p class="card-text">NotiTorti es un sitio de noticias en constante mejora y actualizacion que 
+    trae las noticas mas relevantes de la forma mas rapida y eficaz posible.
+    </p>
+    <h5 class="card-title">Historia</h5>
+    <p class="card-text">NotiTorti tiene 5 minutos en el negocio ofreciendo lo mejor de lo mejor en informacion.
+    </p>
+  </div>
+</div>
+
+
+<div class="card-group">
+  <div class="card">
+    <img src="230px-Nicolás_Maduro_crop_2015.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Javier Alatorre</h5>
+      <p class="card-text">Periodista</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="migueldecervantes-56a5ab363df78cf7728955bd.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Miguel de Cervantes</h5>
+      <p class="card-text">Redactor</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="220px-LinuxCon_Europe_Linus_Torvalds_03_(cropped).jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Linus Torvalds</h5>
+      <p class="card-text">Programador/Diseñador</p>
+    </div>
+  </div>
+</div>
+
+
+<!-- Video -->
+<script>
+function muestraVideo(y) {
+  var x = document.getElementById("videoDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    y=0;
+    x.style.display = "none";
+    a.style.display = "none";
+  }
+  window.scrollTo(0, y);
+}
+</script>
+
+<script>
+function muestraOnda(y) {
+  var x = document.getElementById("sismoDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    y=0;
+    x.style.display = "none";
+  }
+  window.scrollTo(0, y);
+}
+</script>
+
+<script>
+function muestraElon(y) {
+  var x = document.getElementById("animeDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    y=0;
+    x.style.display = "none";
+  }
+  window.scrollTo(0, y);
+}
+</script>
+
+<!-- css -->
+<style>
+html {
+  background: white;
+}
+
+.popout {
+  font-family: Futura, sans-serif;
+  font-weight: 900;
+  font-size: 80px;
+  padding: 80px;
+}
+@-webkit-keyframes ani {
+  0% {
+    -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+    text-shadow: 0em 0em 0 lightblue;
+    color: black;
+  }
+  30% {
+    -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+    text-shadow: 0em 0em 0 lightblue;
+    color: black;
+  }
+  70% {
+    -webkit-transform: translate3d(0.08em, -0.08em, 0);
+            transform: translate3d(0.08em, -0.08em, 0);
+    text-shadow: -0.08em 0.08em lightblue;
+    color: black;
+  }
+  100% {
+    -webkit-transform: translate3d(0.08em, -0.08em, 0);
+            transform: translate3d(0.08em, -0.08em, 0);
+    text-shadow: -0.08em 0.08em lightblue;
+    color: black;
+  }
+}
+@keyframes ani {
+  0% {
+    -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+    text-shadow: 0em 0em 0 #C23E3E;
+    color: black;
+  }
+  30% {
+    -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+    text-shadow: 0em 0em 0 #C23E3E;
+    color: black;
+  }
+  70% {
+    -webkit-transform: translate3d(0.08em, -0.08em, 0);
+            transform: translate3d(0.08em, -0.08em, 0);
+    text-shadow: -0.08em 0.08em #C23E3E;
+    color: black;
+  }
+  100% {
+    -webkit-transform: translate3d(0.08em, -0.08em, 0);
+            transform: translate3d(0.08em, -0.08em, 0);
+    text-shadow: -0.08em 0.08em #C23E3E;
+    color: black;
+  }
+}
+.popout span {
+  position: relative;
+  display: inline-block;
+  -webkit-animation: ani 1s infinite alternate cubic-bezier(0.86, 0, 0.07, 1);
+          animation: ani 1s infinite alternate cubic-bezier(0.86, 0, 0.07, 1);
+}
+.popout span:nth-last-child(1n) {
+  -webkit-animation-delay: -0.1666666667s;
+          animation-delay: -0.1666666667s;
+}
+.popout span:nth-last-child(2n) {
+  -webkit-animation-delay: -0.3333333333s;
+          animation-delay: -0.3333333333s;
+}
+.popout span:nth-last-child(3n) {
+  -webkit-animation-delay: -0.5s;
+          animation-delay: -0.5s;
+}
+
+</style>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
